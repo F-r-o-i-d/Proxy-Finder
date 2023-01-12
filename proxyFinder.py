2701c1db2aa4f586455648c68455f801080f79de
F-r-o-i-d <82611242+F-r-o-i-d@users.noreply.github.com>
2023-01-12 13:01:26 +0100

    Add files via upload

--- proxyFinder.py
+++ proxyFinder.py
@@ -1,30 +1,40 @@
 import random
 import requests
 import time
-import socket
-import os, threading
+import socket, base64
+import os, threading, sys
 
-def GenerateRandomIp():
-    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+def GenerateRandomIp(baseIP):
+    baseIPlen = len(baseIP.split("."))
+    
+    ip = ""
+    for i in range(4 - baseIPlen):
+        ip += str(random.randint(0, 255)) + "."
+    
+    ip = ip[:-1]
+    ip = baseIP + "." + ip
     return ip
 
 #ip echo api
 apis = []
-apis.append("http://ip.chinaz.com/getip.aspx")
-apis.append("http://ip.taobao.com/service/getIpInfo.php?ip=myip")
 apis.append("http://ip-api.com/json/")
-apis.append("http://ip.ws.126.net/ipquery")
-apis.append("http://ip.360.cn/IPShare/info")
-apis.append("http://ip138.com/ips1388.asp?ip=")
-apis.append("http://ip.taobao.com/service/getIpInfo.php?ip=")
+apis.append("https://user.ip138.com/ip/")
 apis.append("http://www.net.cn/static/customercare/yourip.asp")
 apis.append("http://ip.chinaz.com/getip.aspx")
-apis.append("http://pv.sohu.com/cityjson?ie=utf-8")
-
+apis.append("https://ipecho.net/extra")
+apis.append("https://api.ipify.org?format=json")
+apis.append("https://api-ipv4.ip.sb/ip")
+apis.append("https://ip4.seeip.org")
+apis.append("http://api.ipaddress.com/myip?format=json&callback=myCallback")
+try:
+    isDebug = sys.argv[1] 
+    isDebug = True
+except:
+    isDebug = False
 
 def CheckProxy(proxy, timeout=5):
     try:
-        proxy = {'http': proxy}
+        proxy = {'https': proxy, 'http': proxy}
         r = requests.get(random.choice(apis), proxies=proxy, timeout=timeout)
         if r.status_code == 200:
             if proxy.split(':')[0] in r.text:
@@ -45,7 +55,7 @@ def check_if_alive(proxy):
         return False
 
 def sendwebhookMessage(message):
-    webhook_url = "https://discord.com/api/webhooks/1063023261086126130/7aw6bWH5mGfXwIGpyYSYBGDMwcST9YaG_VB7aXwJTAcX-oLL2qhnymGMaEGoqv3nfVvT"
+    webhook_url = base64.b64decode("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA2MzAyMzI2MTA4NjEyNjEzMC83YXc2YldINW1HZlh3SUdweVlTWUJHRE13Y1NUOVlhR19WQjdhWHdKVEFjWC1vTEwycWhueW1HTWFFR29xdjNuZlZ2VA==")
     data = {"content": message}
     result = requests.post(webhook_url, json=data)
     try:
@@ -54,11 +64,13 @@ def sendwebhookMessage(message):
         print(err)
     else:
         pass
-
+totalProxies = 0
 def GetProxy():
     while True:
+        baseIP = ["23.229.80", "51", "163.116.248", "51.210"]
         common_ports = [8080, 3128, 1080, 6588, 8081, 8081, 8090, 13128]
-        ip = GenerateRandomIp()
+        ip = GenerateRandomIp(random.choice(baseIP))
+        print(ip)
         proxys = [ip + ":" + str(port) for port in common_ports]
         for proxy in proxys:
             is_alive = check_if_alive(proxy)
@@ -69,15 +81,15 @@ def GetProxy():
                 elif CheckProxy(proxy) == 2:
                     sendwebhookMessage(proxy + "Anonymous / Elite Proxy")
                     print("+" + proxy)
-                else:
+                elif isDebug:
                     print("/" + proxy)
-            else:
+            elif isDebug:
                 print("-" + proxy)
 
 if __name__ == '__main__':
     sendwebhookMessage("Proxy Finder Started")
 
-    for i in range(10):
+    for i in range(100):
         t = threading.Thread(target=GetProxy)
         t.start()
     while(1):
